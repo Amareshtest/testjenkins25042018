95388e8d61a34c4a99688a4d0806e91eaeb2cb38
Amareshtest <amaresh2014@gmail.com>
2018-04-25 17:08:05 +0530

    First commit

---
+++ jenkinstesting/src/test/java/Testcase/NewTC01.java
@@ -0,0 +1,12 @@
+package Testcase;
+
+import org.testng.annotations.Test;
+
+public class NewTC01 {
+	@Test
+	public static void testcase01()
+	{
+		System.out.println("firsttestcase");
+	}
+
+}
---
+++ jenkinstesting/src/test/java/Testcase/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author PTN
+ *
+ */
+package Testcase;
